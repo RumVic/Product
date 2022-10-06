6f93f25777d70f8375168dc64484fba4c6f232bd
VicRum <vicrum93@gmail.com>
2022-10-06 09:46:15 +0300

    [AddingProductServlet][web.xml]

--- src/main/java/by/itacademy/jd2/Mk_92_22_Product/controllers/AddingProductServlet.java
+++ src/main/java/by/itacademy/jd2/Mk_92_22_Product/controllers/AddingProductServlet.java
@@ -1,6 +1,5 @@
 package by.itacademy.jd2.Mk_92_22_Product.controllers;
 
-import by.itacademy.jd2.Mk_92_22_Product.entity.ProductBuilder;
 import by.itacademy.jd2.Mk_92_22_Product.storage.ProductStorage;
 
 import javax.servlet.ServletException;
@@ -11,18 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(name = "AddingProductServlet",urlPatterns = "/add")
+@WebServlet(name = "AddingProductServlet", urlPatterns = "/add")
 public class AddingProductServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IllegalArgumentException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
         req.getRequestDispatcher("/WEB-INF/view/addProduct.jsp").forward(req, resp);
 
     }
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+
 
         String idAdd = req.getParameter("id");
         String nameAdd = req.getParameter("name");
@@ -30,13 +32,20 @@ public class AddingProductServlet extends HttpServlet {
         String saleAdd = req.getParameter("sale");
         String aboutAdd = req.getParameter("about");
 
+
         if(idAdd!=null && nameAdd!=null && priceAdd!=null && saleAdd!=null && aboutAdd!=null) {
+         /*   if (idAdd.isBlank()) {
+                throw new NumberFormatException("we hate white");
+            }*/
+
             int i = Integer.parseInt(idAdd);
             int p = Integer.parseInt(priceAdd);
             int s = Integer.parseInt(saleAdd);
-            ProductStorage.getInstance().add(i, nameAdd, p, s, aboutAdd);
-            resp.sendRedirect("http://localhost:8080/Mk-JD2-92-22-Product-1.0-SNAPSHOT/get");
 
-        }//else throw IllegalArgumentException ("You dint pass parameters");
+            ProductStorage.getInstance().add(i, nameAdd, p, s, aboutAdd);
+            resp.sendRedirect(req.getContextPath()+"/get");
+            //resp.sendRedirect("http://localhost:8080/Mk-JD2-92-22-Product-1.0-SNAPSHOT/get");
+
+        }else throw new ServletException("GET method is not supported.");
     }
 }
